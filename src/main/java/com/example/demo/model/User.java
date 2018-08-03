e27339eb8dd89176e423a78954a77d9b94083816
Marcin Krason <marcin.krason@ttpsc.pl>
2018-08-03 23:13:04 +0200

    changed @GeneratedValue(strategy = GenerationType.IDENTITY from AUTO

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 public class User implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "firstname")
